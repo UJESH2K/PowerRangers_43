4edffc6ea90573523ec973844b0225b9d4eef219
Prince Kumar <pk7411956419@gmail.com>
2024-09-14 09:50:51 +0530

    Added zod validation

---
+++ common/types.js
@@ -0,0 +1,24 @@
+const z = require("zod");
+
+const signUp = z.object({
+  userName: z.string().email(),
+  firstName: z.string(),
+  lastName: z.string(),
+  password: z.string(),
+});
+
+const signIn = z.object({
+  userName: z.string().email(),
+  password: z.string(),
+});
+
+const update = z.object({
+  password: z.string().optional(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+});
+
+module.exports = {
+  signUp,
+  signIn,
+};
